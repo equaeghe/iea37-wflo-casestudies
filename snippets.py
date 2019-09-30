e0a1f5c85d1db1142cb53a04be50534ec4c3dcac
Erik Quaeghebeur <E.R,G.Quaeghebeur@tudelft.nl>
2019-09-30 09:56:27 +0200

    improvements for public consumption

--- snippets.py
+++ snippets.py
@@ -3,8 +3,10 @@ import numpy as np
 import IEA37_wflocs_basis as wflocs
 import IEA37_wflocs_optimizers as optz
 import IEA37_wflocs_plotters as pltr
+import matplotlib.pyplot as plt
 
-# import windroses
+
+# %%  import windroses
 with open('IEA37-wind_resource.case_study.yaml') as f:
     props = yaml.safe_load(f)
 wind_dirs = np.array(props['variables']['wind_direction'])
@@ -12,28 +14,27 @@ wind_freq = np.array(props['probability'])
 
 downwind_vectors = wflocs.downwind_vector(wind_dirs)
 
-# plotting windroses
-import matplotlib.pyplot as plt
-
+# %% plotting windroses
 fig = plt.figure()
 ax = fig.add_subplot(111, polar=True)
 ax.set_aspect(1.0)
 ax.set_theta_zero_location("N")
 ax.set_theta_direction(-1)
 
-ax.set_ylim(0,0.225)
+ax.set_ylim(0, 0.225)
 
-ax.bar(wind_dirs / 360 * 2 * pi, wind_freq, color='b', width=2*pi/16)
+ax.bar(wind_dirs / 360 * 2 * np.pi, wind_freq, color='b', width=2*np.pi/16)
 
-# import parameters
+# %% import parameters
 rotor_diameter = 130
 wind_speed = 9.8
-turb_ci, turb_co, rated_ws, rated_pwr, turb_diam = wflocs.getTurbAtrbtYAML('IEA37-wind_turbine.onshore_reference.yaml')
+turb_ci, turb_co, rated_ws, rated_pwr, turb_diam = wflocs.getTurbAtrbtYAML(
+        'IEA37-wind_turbine.onshore_reference.yaml')
 wind_speed /= rated_ws
 turb_ci /= rated_ws
 turb_co /= rated_ws
 
-# importing initial layout
+# %% importing initial layout
 with open('IEA37-optimization_case_study_IO.ex36.yaml', 'r') as f:
     props = yaml.safe_load(f)
 site_radius = 2000
@@ -46,10 +47,11 @@ turb_coords.x /= rotor_diameter
 turb_coords.y /= rotor_diameter
 layout = np.copy(turb_coords).view(np.recarray)
 
-# optimize layout
+# %% optimize layout
 new_layout = optz.search_equilibrium(layout, site_radius / rotor_diameter, 100,
                                      downwind_vectors, wind_freq,
                                      wind_speed, turb_ci, turb_co)
 
-# plot layout
+# %% plot layout
+plt.figure()
 pltr.layout_scatter(layout, new_layout, site_radius / rotor_diameter)
